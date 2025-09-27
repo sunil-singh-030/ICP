550ff2ba80db5767d70d6926b9263ca633d9cb0b
Sunil Singh <sunil.singh.030250@gmail.com>
2025-09-27 06:36:28 +0530

    Add files via upload

---
+++ Day-1-26-sep/0239-sliding-window-maximum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int [] ans = new int[nums.length-k+1];
+        int num=Integer.MIN_VALUE;
+        int ind=-1;
+        for (int i=0;i<k;i++){
+            if (nums[i]>=num){
+                ind=i;
+                num=nums[i];
+            }
+        }
+        ans[0]=num;
+        for (int i=1;i<nums.length-k+1;i++){
+            if (nums[i+k-1]>=num){
+                num=nums[i+k-1];
+                ind=i+k-1;
+                ans[i]=num;
+            }
+            else if (ind!=i-1){
+                ans[i]=num;
+            }
+            else{
+                num=Integer.MIN_VALUE;
+                ind=-1;
+                for (int j=i;j<=i+k-1;j++){
+                    if (nums[j]>=num){
+                        num=nums[j];
+                        ind=j;
+                    }
+                }
+                ans[i]=num;
+            }
+
+        }
+        return ans;
+    }
+}
