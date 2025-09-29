7264897b2b45ed926fd0b91c5f090616f2bedd0d
Sunil Singh <sunil.singh.030250@gmail.com>
2025-09-29 06:36:25 +0530

    Add files via upload

---
+++ Day-2-27-sep/367-valid-perfect-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int x=num;
+        long lo=1;
+        long hi=x;
+        long out=1;
+        while (lo<=hi){
+            long mid=(lo+hi)/2;
+            if (Math.pow(mid,2)<=x){
+                out=mid;
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+        //System.out.println(out);
+        return ((int) Math.pow(out,2))==x;
+    }
+}
