4464d15beef77164c9ea9af2584b219032a59a8a
Sunil Singh <sunil.singh.030250@gmail.com>
2025-09-30 07:37:45 +0530

    Add files via upload

---
+++ Day-3-29-sep/74-search-a-2d-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int left=0;
+        int right=matrix[0].length-1;
+        while (left<matrix.length && right>=0){
+            int num=matrix[left][right];
+            if (num==target){
+                return true;
+            }
+            else{
+                if (num>target){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return false;
+    }
+}
