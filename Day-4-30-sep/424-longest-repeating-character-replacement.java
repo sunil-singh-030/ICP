8a0a4b8bd67e68aeace45166414bb13869c4ad98
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-02 08:28:56 +0530

    Create 424-longest-repeating-character-replacement.java

---
+++ Day-4-30-sep/424-longest-repeating-character-replacement.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int [] arr=new int[26];
+        for (int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'A']++;
+        }
+        int ans=1;
+        for (int i=0;i<26;i++){
+            if (arr[i]>0){
+                int out=countsamelenstr(s,k,(char) (i+65));
+                ans=Math.max(ans,out);
+            }
+        }
+        return ans;
+    }
+    public static int countsamelenstr(String s,int k,char ch){
+        int ans=1;
+        int si=0;
+        int ci=0;
+        int flip=0;
+        while (ci<s.length()){
+            // grow
+            if (s.charAt(ci)!=ch){
+                flip++;
+            }
+            // shrink
+            while (flip>k){
+                if (s.charAt(si)!=ch){
+                    flip--;
+                }
+                si++;
+            }
+            // ans update
+            ans=Math.max(ans,ci-si+1);
+            ci++;
+        }
+        return ans;
+    }
+}
