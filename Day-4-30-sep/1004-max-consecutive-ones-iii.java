fd0717f284876c4f886bdf97c63aa09f20d8c7fa
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-02 08:29:31 +0530

    Create 1004-max-consecutive-ones-iii.java

---
+++ Day-4-30-sep/1004-max-consecutive-ones-iii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int flip=0;
+        int start=0;
+        int ind=0;
+        int ans=0;
+        while (ind<nums.length){
+            if (nums[ind]==1){
+                ind++;
+            }
+            else{
+                if (flip<k){
+                    ind++;
+                    flip++;
+                }
+                else{
+                    ans=Math.max(ans,ind-start);
+                    while (flip==k){
+                        if (nums[start]==0){
+                            flip--;
+                        }
+                        start++;
+                    }
+                }
+            }
+        }
+        ans=Math.max(ans,ind-start);
+        return ans;
+    }
+}
