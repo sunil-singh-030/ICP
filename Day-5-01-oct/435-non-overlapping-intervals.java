bc027a3d6a775d7ebc7a3e4fed79f8d1503233d9
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-02 08:01:07 +0530

    Add files via upload

---
+++ Day-5-01-oct/435-non-overlapping-intervals.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+        Arrays.sort(intervals ,(a,b) -> Integer.compare(a[1],b[1]));
+        int req = 0;
+        int end = intervals[0][1];
+        for (int i=1 ; i<n ; i++){
+            if (intervals[i][0]<end){
+                req++;
+            }
+            else{
+                end = intervals[i][1];
+            }
+        }
+        return req;
+    }
+}
