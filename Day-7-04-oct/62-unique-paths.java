9cc2914866425483f498c4d45a1b1300d3b95786
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-05 07:26:37 +0530

    Create 62-unique-paths.java

---
+++ Day-7-04-oct/62-unique-paths.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for (int i=m-1 ; i>=0 ; i--){
+            for (int j=n-1 ; j>=0 ; j--){
+                if (j!=n-1){
+                    dp[j] = dp[j+1] + dp[j];
+                    continue;
+                }
+                if (i==m-1 && j==n-1){
+                    dp[j] = 1;
+                    continue;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
